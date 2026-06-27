5c1633b6f9df596d02f7477f62ec7343886f4b7e
Rafidul01 <rafidulhasan555@gmail.com>
2026-06-28 01:09:51 +0600

    jwt implimented for access and resfresh token

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,9 +11,9 @@ export default {
     app_url : process.env.APP_URL,
     bcrypt_salt_rounds : process.env.BCRYPT_SALT_ROUNDS,
     jwt : {
-        secret : process.env.JWT_SECRET,
-        refresh_secret : process.env.JWT_REFRESH_SECRET,
-        expires_in : process.env.JWT_EXPIRES_IN,
-        refresh_expires_in : process.env.JWT_REFRESH_EXPIRES_IN
+        secret : process.env.JWT_SECRET!,
+        refresh_secret : process.env.JWT_REFRESH_SECRET!,
+        expires_in : process.env.JWT_EXPIRES_IN! as string,
+        refresh_expires_in : process.env.JWT_REFRESH_EXPIRES_IN! as string
     }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,11 @@
 import { ILoginUserPayload } from "./auth.interface";
 import { prisma } from "../../lib/prisma";
 import bcrypt from "bcryptjs";
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
+
+
 
 const loginUser = async (payload: ILoginUserPayload) => {
   const { email, password } = payload;
@@ -17,7 +22,26 @@ const loginUser = async (payload: ILoginUserPayload) => {
     throw new Error("Password does not match");
   }
 
-  return user;  
+  const jwtPayload = {
+    userId: user.id,
+    email: user.email,
+    name: user.name,
+    role: user.role
+  };
+  
+
+//   const accessToken = jwt.sign(jwtPayload,config.jwt.secret,{
+//     expiresIn: config.jwt.expires_in
+//   } as SignOptions);
+
+  const accessToken = jwtUtils.createToken(jwtPayload,config.jwt.secret, config.jwt.expires_in as SignOptions);
+
+  const refreshToken = jwtUtils.createToken(jwtPayload,config.jwt.refresh_secret, config.jwt.refresh_expires_in as SignOptions);
+  
+  return {
+    accessToken,
+    refreshToken
+  };  
 };
 
 export const authService = {
